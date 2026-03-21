751748fd9eea761fe3a879d02945c5ed66679293
MuhammadMasab002 <muhammadmasab2002@gmail.com>
2026-03-21 23:59:24 +0500

    feat: error handler and middleware

---
+++ mocco-backend/src/middlewares/error.middleware.js
@@ -0,0 +1,37 @@
+import ErrorHandler from "../utils/ErrorHandler.js";
+
+const errorMiddleware = (err, req, res, next) => {
+  console.error("Error:", err);
+
+  err.statusCode = err.statusCode || 500;
+  err.message = err.message || "Internal Server Error";
+
+  //   wrong Mongoose Object ID error
+  if (err.name === "CastError") {
+    const message = `Resource not found. Invalid: ${err.path}`;
+    err = new ErrorHandler(message, 400);
+  }
+  //   Mongoose duplicate key error
+  if (err.code === 11000) {
+    const message = `Duplicate ${Object.keys(err.keyValue)} entered`;
+    err = new ErrorHandler(message, 400);
+  }
+  //   wrong JWT error
+  if (err.name === "JsonWebTokenError") {
+    const message = "JSON Web Token is invalid. Try again!!!";
+    err = new ErrorHandler(message, 400);
+  }
+  //   JWT expired error
+  if (err.name === "TokenExpiredError") {
+    const message = "JSON Web Token is expired. Try again!!!";
+    err = new ErrorHandler(message, 400);
+  }
+
+  //   response
+  res.status(err.statusCode).json({
+    success: false,
+    message: err.message,
+  });
+};
+
+export default errorMiddleware;
